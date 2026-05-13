a1abe4acb3452645f0557162bb2787daa6247d9c
Witchie <73515621+Faqpy@users.noreply.github.com>
2026-05-13 22:07:39 +0530

    Clean up about page content and scripts

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,13 +1,7 @@
 <main>
     <p style="font-size: 80px; font-style: normal;"><span><a class="titleSpan" href="/">witchie</a></span> です!</p>
     <p>Heya, witchie here! I'm uhh a really mischievous person I would say.</p>
-    <p>I also run a Hoshikawa Sara Fan Club! <CustomEmoji src={saraHappi} name=":SaraHappi:"/><CustomEmoji src={saraHappi} name=":SaraHappi:"/><CustomEmoji src={saraHappi} name=":SaraHappi:"/></p>
-    <p><span><a href="https://discord.gg/VQBfmCxeqZ" target="_blank" class="titleSpan" style="text-decoration: underline;">星川サラ•ファンクラブ</a></span></p>
 </main>
 <footer>
     <p>i cnat cod &nbsp;•&nbsp; witchie</p>
 </footer>
-<script>
-    import CustomEmoji from "$lib/customEmoji.svelte";
-    const saraHappi = "https://cdn.discordapp.com/emojis/1490319676931051571.webp?quality=lossless"
-</script>
