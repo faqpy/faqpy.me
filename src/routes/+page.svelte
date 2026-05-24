1b1b336560743726199daa573b453840ff0fb003
faqpy <73515621+Faqpy@users.noreply.github.com>
2026-05-24 16:56:35 +0530

    some temporary changes I want to make (not sure about keeping them)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,10 @@
     <p style="font-size: 80px; font-style: normal;"><span><a class="titleSpan" href="/about">faqpy</a></span> です!</p>
     <p style="font-size: 20px; font-style: normal;">(a.k.a <span style="text-decoration: line-through;">witchie</span>)</p>
     <p>Hello, I am faqpy! Thanks for visiting! <CustomEmoji src={miauWhat} name=":miauWhat:"/> <CustomEmoji src={fw} name=":fw:"/></p>
-    <p>I'm a self-taught web developer (currently learning) from India.</p>
+    <p>I'm a self-taught web developer (currently learning).</p>
     <a href="https://bsky.app/profile/faqpy.me" target="_blank"><button class="socialBtns">BSky</button></a>
     <a href="mailto:faqpy@proton.me" target="_blank"><button class="socialBtns">Mail</button></a>
-    <a href="https://discord.com/users/600698629501616138" target="_blank"><button
+    <a href="https://discord.com/users/277867160796397568" target="_blank"><button
             class="socialBtns">Discord</button></a>
 </main>
 <footer>
