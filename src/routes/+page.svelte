53e3e16bcfdd7ffd2f48d0510d4a5a59f645f450
faqpy <73515621+Faqpy@users.noreply.github.com>
2026-05-30 18:12:33 +0530

    revert these title changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <main>
     <p style="font-size: 80px; font-style: normal;"><span><a class="title-span" href={resolve("/about")}>faqpy</a></span> です!</p>
     <p style="font-size: 20px; font-style: normal;">(a.k.a witchie)</p>
-    <p>Heya, I am faqpy! <CustomEmoji src={miauWhat} name=":miauWhat:"/> <CustomEmoji src={fw} name=":fw:"/></p>
-    <p>I'm a self-taught web developer (currently learning).</p>
-    <a href="https://bsky.app/profile/faqpy.me" target="_blank" title="Rarely used."><button class="social-btns">BSky</button></a>
-    <a href="mailto:faqpy@proton.me" target="_blank" title="Choose b/w Mail and Contact according to your suitability."><button class="social-btns">Mail</button></a>
-    <a href="https://discord.com/users/277867160796397568" target="_blank" title="Discord is often preferred for faster replies."><button class="social-btns">Discord</button></a>
-    <a href={resolve("/contact")} title="Choose b/w Mail and Contact according to your suitability."><button class="social-btns">Contact</button></a>
+    <p>Heya, I am faqpy! <CustomEmoji src={miauWhat} name=":miauWhat:"/></p>
+    <p>I'm a self-taught web developer (currently learning)</p>
+    <a href="https://bsky.app/profile/faqpy.me" target="_blank"><button class="social-btns">BSky</button></a>
+    <a href="mailto:faqpy@proton.me" target="_blank"><button class="social-btns">Mail</button></a>
+    <a href="https://discord.com/users/277867160796397568" target="_blank" ><button class="social-btns">Discord</button></a>
+    <a href={resolve("/contact")}><button class="social-btns">Contact</button></a>
 </main>
 <footer>
     <p>i cnat cod &nbsp;•&nbsp; witchie</p>
@@ -15,5 +15,4 @@
     import { resolve } from "$app/paths";
     import CustomEmoji from "$lib/customEmoji.svelte";
     const miauWhat = "https://cdn.discordapp.com/emojis/1280886209635291137.webp?quality=lossless"
-    const fw = "https://cdn.discordapp.com/emojis/1485755012511694999.webp?quality=lossless"
 </script>
