e6c6d62b692619274596d3aefd2f7e7fcdc6d7e6
faqpy <73515621+Faqpy@users.noreply.github.com>
2026-06-05 04:23:42 +0530

    minor changes to colors and transition properties

--- src/lib/baseLayoutFooter.svelte
+++ src/lib/baseLayoutFooter.svelte
@@ -3,15 +3,18 @@
 </script>
 <footer>
     <div class="buttons">
-        <a href="mailto:hi@faqpy.me" target="_blank" rel="noopener noreferrer">
-            <button class="footer-social-btn" title="mail me">
-                <Icon icon="material-symbols:mail-outline" class="btn-icon" />
-            </button>
+        <a href="mailto:hi@faqpy.me" class="footer-social-btn" title="mail me" target="_blank" rel="noopener noreferrer">
+            <Icon icon="material-symbols:mail-outline" class="btn-icon" />
         </a>
     </div>
     <p>&nbsp;•&nbsp;faqpy.me</p>
 </footer>
 <style>
+    :root {
+        --ws-d1: hsl(343, 90%, 81%);
+        --ws-d3: hsl(316, 16%, 28%);
+        --ws-d5: hsl(32, 84%, 88%);
+    }
     footer {
         font-family: 'CalSans', sans-serif;
         font-size: 14px;
@@ -20,7 +23,7 @@
         width: 100%; 
         padding: 24px 16px; 
         box-sizing: border-box;
-        display: inline-flex;
+        display: flex;
         flex-direction: row;
         align-items: center;
         justify-content: center;
@@ -29,16 +32,26 @@
     footer .footer-social-btn {
         width: 44px;
         height: 44px;
-        padding: 0;
-        border-radius: 50%; 
+        border-radius: 999px;
+        display: flex; 
+        align-items: center;
         justify-content: center; 
-        border: none;
+        text-decoration: none;
+        background-color: var(--ws-d3);
+        transition: background-color 0.25s ease;
     }
     footer .footer-social-btn :global(.btn-icon) {
         width: 24px; 
         height: 24px;
         flex-shrink: 0;
-        color: whitesmoke;
         display: inline-block;
+        color: var(--ws-d5);
+        transition: color 0.2s ease;
+    }
+    footer .footer-social-btn:hover {
+        background-color: var(--ws-d1);
+    }
+    footer .footer-social-btn:hover :global(.btn-icon) {
+        color: var(--ws-d3);
     }
 </style>
